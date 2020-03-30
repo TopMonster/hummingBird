dc6372b54a72048fb8559f0a9dba98c838a106a8
Top <Top@LAPTOP-2OO90RE0>
2020-03-30 18:09:16 +0800

    调整初始化sql

--- hummingbird-api/db/sqlserver.sql
+++ hummingbird-api/db/sqlserver.sql
@@ -9,17 +9,6 @@ CREATE TABLE tb_user (
   UNIQUE (username)
 );
 
-
--- 用户Token表
-CREATE TABLE tb_token (
-  user_id bigint NOT NULL,
-  token varchar(100) NOT NULL,
-  expire_time datetime,
-  update_time datetime,
-  PRIMARY KEY (user_id),
-  UNIQUE (token)
-);
-
 -- 账号：13612345678  密码：admin
 INSERT INTO tb_user (username, mobile, password, create_time) VALUES ('mark', '13612345678', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '2017-03-23 22:37:41');
 
